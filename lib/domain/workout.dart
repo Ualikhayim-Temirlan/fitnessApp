c72fc50ddd2243c965442f576e7449f7887068e8
akniyetakkos <202242064+simTemirlan@users.noreply.github.com>
2025-04-29 23:07:11 +0500

    Initial commit

---
+++ lib/domain/workout.dart
@@ -0,0 +1,13 @@
+class Workout {
+  final String title;
+  final String author;
+  final String description;
+  final String level;
+
+  Workout({
+    required this.title,
+    required this.author,
+    required this.description,
+    required this.level,
+  });
+}
